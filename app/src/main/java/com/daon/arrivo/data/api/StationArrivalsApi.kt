792b514c6abd7c9c01fa8bb680d58c3ccb69951f
daon1213 <diaeiyo1213@naver.com>
2023-04-20 13:41:28 +0900

    🔧:: Station Arrival API

---
+++ app/src/main/java/com/daon/arrivo/data/api/StationArrivalsApi.kt
@@ -0,0 +1,13 @@
+package com.daon.arrivo.data.api
+
+import com.daon.arrivo.BuildConfig
+import com.daon.arrivo.data.api.response.RealtimeStationArrivals
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface StationArrivalsApi {
+
+    @GET("api/subway/${BuildConfig.SEOUL_API_ACCESS_KEY}/json/realtimeStationArrival/0/100/{stationName}")
+    suspend fun getRealtimeStationArrivals(@Path("stationName") stationName: String): Response<RealtimeStationArrivals>
+}
